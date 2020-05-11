19855dd8d915e2c4600950f8acf127f6ad27ee47
ttnppedr <priest0204@gmail.com>
2020-05-11 21:24:43 +0800

    use service provider and facade

---
+++ src/Facades/ChuckNorris.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Ttnppedr\ChuckNorrisJokes\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class ChuckNorris extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'chuck-norris';
+    }
+}
